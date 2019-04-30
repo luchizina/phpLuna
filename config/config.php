b84ff64cabd611d23afb6b41f65114f5a0c51df0
Lucia <luciaurrutty@gmail.com>
2019-04-29 21:14:36 -0300

    Un commit

---
+++ config/config.php
@@ -0,0 +1,16 @@
+<?php
+	define("DB_HOST", "localhost");
+	define("DB_USR", "root");
+	define("DB_PASS", "hola");
+	define("DB_DB", "framework");
+	//define(DB_TYPE, "mysql");
+
+	$template_config = 
+    array(
+        'template_dir' => 'vistas/',
+        'compile_dir' => 'libs/smarty/templates_c/',
+        'cache_dir' => 'libs/smarty/cache/',
+        'config_dir' => 'libs/smarty/configs/',
+        );
+    define ("URL_BASE","/ejemplos/framework/");
+?>
