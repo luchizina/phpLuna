2eade6a3166052234094e9a70d4f7974bd932954
Nazareno Alvarez <Aeliner123@gmail.com>
2019-05-06 19:31:55 -0300

    new file:   controladores/ctrl_propuesta.php

---
+++ controladores/ctrl_propuesta.php
@@ -0,0 +1,11 @@
+<?php  
+require "clases/clase_base.php";
+require "clases/propuesta.php";
+require "clases/estado.php";
+require "clases/list_estado.php";
+require "clases/recompensa.php";
+require_once('clases/template.php');
+require_once('clases/Utils.php');
+require_once('clases/session.php');
+require_once('clases/auth.php');
+?>
