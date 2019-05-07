174c9eba823575909c6de8f50e10bf84354040ee
nahuel062 <nambroa@gmail.com>
2019-05-07 17:07:54 -0300

    modified:   controladores/ctrl_usuario.php

--- controladores/ctrl_usuario.php
+++ controladores/ctrl_usuario.php
@@ -52,5 +52,70 @@ class ControladorUsuario extends ControladorIndex {
        $tpl->mostrar('usuarios_listado',$datos);
    
    }
+
+
+function listadoMovil($params=array()){
+
+   $buscar="";
+       $titulo="Listado";
+       $mensaje="";
+       if(!empty($params)){
+           if($params[0]=="borrar"){
+               $usuario=new Usuario();
+               $idABorrar=$params[1];
+                if($usuario->borrar($idABorrar)){
+                    //Redirigir al listado
+                    //header('Location: index.php');exit;
+                    $this->redirect("usuario","listado");
+                }else{
+                    //Mostrar error
+                    $usr=$usuario->obtenerPorId($idABorrar);
+                    //$mensaje="Error!! No se pudo borrar el usuario  <b>".$usr->getNombre()." ".$usr->getApellido()."</b>";
+                    $mensaje="ERROR. No existe el usuario";
+                    $usuarios=$usuario->getListado(); 
+                }
+           }else{
+                $usuario=new Usuario();
+               $usuarios=$usuario->getListado();  
+            }
+       }else{
+            $usuario=new Usuario();
+           $usuarios=$usuario->getListado();  
+        }
+       
+     
+      /*  $datos = array(
+       'usuarios' => $usuarios,
+       'buscar' => $buscar,
+       'titulo' => $titulo,
+       'mensaje' => $mensaje,
+       );*/
+
+       $arreglo=["status"=>"ok","message"=>$usuarios];
+
+
+       $listaUsers= json_encode($arreglo);
+       //var_dump($listaUsers);
+
+       echo  $listaUsers;
+
+
+}
+
+
+
+function multiplicidad($a = array())
+{
+  $f = $a[0]*100;
+  $c=["numerito"=>$f];
+$b=["status"=>"ok","message"=>$c];
+$hola= json_encode($b);
+echo $hola;
+}
+
+
+
+
+
 }
 ?>
