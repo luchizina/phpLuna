fe4e2cca4869361982267937ae7a1d1de176e58b
Lucia <luciaurrutty@gmail.com>
2019-05-14 19:31:52 -0300

    Commit

--- libs/smarty/templates_c/fb05f10fe29da8a558e3c965866d6cd5dd601380.file.cabezal.tpl.php
+++ libs/smarty/templates_c/fb05f10fe29da8a558e3c965866d6cd5dd601380.file.cabezal.tpl.php
@@ -1,4 +1,4 @@
-<?php /* Smarty version Smarty-3.1.21-dev, created on 2019-05-14 23:21:20
+<?php /* Smarty version Smarty-3.1.21-dev, created on 2019-05-15 00:31:27
          compiled from "vistas\cabezal.tpl" */ ?>
 <?php /*%%SmartyHeaderCode:9213224335cdb3150003b51-22822232%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
 $_valid = $_smarty_tpl->decodeProperties(array (
@@ -7,7 +7,7 @@ $_valid = $_smarty_tpl->decodeProperties(array (
     'fb05f10fe29da8a558e3c965866d6cd5dd601380' => 
     array (
       0 => 'vistas\\cabezal.tpl',
-      1 => 1557797996,
+      1 => 1557873083,
       2 => 'file',
     ),
   ),
@@ -15,16 +15,17 @@ $_valid = $_smarty_tpl->decodeProperties(array (
   'function' => 
   array (
   ),
+  'version' => 'Smarty-3.1.21-dev',
+  'unifunc' => 'content_5cdb3150040a53_84411685',
   'variables' => 
   array (
     'usuLogNick' => 0,
     'url_login' => 0,
+    'url_regis' => 0,
     'url_logout' => 0,
     'usuLogueado' => 0,
   ),
   'has_nocache_code' => false,
-  'version' => 'Smarty-3.1.21-dev',
-  'unifunc' => 'content_5cdb3150040a53_84411685',
 ),false); /*/%%SmartyHeaderCode%%*/?>
 <?php if ($_valid && !is_callable('content_5cdb3150040a53_84411685')) {function content_5cdb3150040a53_84411685($_smarty_tpl) {?>
  <head>
@@ -54,6 +55,8 @@ $_valid = $_smarty_tpl->decodeProperties(array (
                <?php if ($_smarty_tpl->tpl_vars['usuLogNick']->value=='') {?>
               <li class="probootstra-cta-button last"><a href="<?php echo $_smarty_tpl->tpl_vars['url_login']->value;?>
 " class="btn btn-primary">INICIAR SESIÓN</a></li>
+              <li class="probootstra-cta-button last"><a href="<?php echo $_smarty_tpl->tpl_vars['url_regis']->value;?>
+" class="btn btn-primary">REGISTRARSE</a></li>
                <?php } else { ?>
                 <li class="probootstra-cta-button last"><a href="<?php echo $_smarty_tpl->tpl_vars['url_logout']->value;?>
 " class="btn btn-primary">CERRAR SESIÓN</a></li>
