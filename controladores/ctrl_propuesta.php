7f3c245e66bcbaa569daaee5d44d0edf4853fdff
Nazareno <Aeliner123@gmail.com>
2019-06-09 21:39:41 -0300

    modified:   clases/propuesta.php 	modified:   clases/recompensa.php 	modified:   controladores/ctrl_propuesta.php

--- controladores/ctrl_propuesta.php
+++ controladores/ctrl_propuesta.php
@@ -339,8 +339,8 @@ public function recursiva($recs, $col, $pos, $prop){
         $col->setRecompensa($r);
         $r->actualizaCant();
         } else {
-          if($pos != 0){
-          $pos = $pos - 1;
+          if($pos != 0){ 
+          $pos--;
           $this->recursiva($recs, $col, $pos, $prop);
         }
       }
@@ -350,10 +350,14 @@ function nuevaColaboracionCel(){
   $Usuario = new Usuario();
   $propuesta = new Propuesta();
   $Recompensa = new Recompensa();
-  $recs = $Recompensa->traerRecompensas($_POST["nombre"]);
-  $usu = $Usuario->obtenerPorMail($_POST['mail']);
-  $prop=$propuesta->obtenerPorNombreProp($_POST["nombre"]);
-   $col->setMonto($_POST["monto"]);
+  //$recs = $Recompensa->traerRecompensas($_POST["nombre"]);
+  //$usu = $Usuario->obtenerPorMail($_POST['mail']);
+  //$prop=$propuesta->obtenerPorNombreProp($_POST["nombre"]);
+   //$col->setMonto($_POST["monto"]);
+  $recs = $Recompensa->traerRecompensas("nueva propb");
+  $usu = $Usuario->obtenerPorMail("nambroa@gmail.com");
+  $prop=$propuesta->obtenerPorNombreProp("nueva propb");
+  $col->setMonto(500);
     $col->setFecha(date("Y-m-d"));
     $col->setUsuario($usu);
     $col->setTituloPropuesta($prop);
@@ -364,15 +368,16 @@ function nuevaColaboracionCel(){
     }
     $this->recursiva($recs, $col, $pos, $prop);
     if($col->agregar()){
-     $prop->setMontoActual($prop->getMontoActual() + $_POST["monto"]);
+     //$prop->setMontoActual($prop->getMontoActual() + $_POST["monto"]);
+      $prop->setMontoActual($prop->getMontoActual() + 500);
       $prop->actualizaMonto();
-      $msg = "que rica ñery te la jugaste";
+      $msg = "que rica compa te la jugaste";
       $array = ["mensajito"=>$msg];
       $arreglo=["status"=>"ok","message"=>[$array]];
             echo json_encode($arreglo);
     }else 
     {
-      $msg = "mal ahí ñery";
+      $msg = "mal ahi compa";
           $array = ["mensajito"=>$msg];
           $arreglo=["status"=>"error","message"=>[$array]];
           echo json_encode($arreglo);
